70f90beeeac1dba9e015c4d0514d6a139b232cfc
mercedesjul <julianbischop@gmail.com>
2019-10-27 22:02:48 +0100

    update

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
-calculate(){
- let desired = document.querySelector('#desired').value;
- let given = document.querySelector('#given').value;
- let try = document.querySelector('#try').value;
- let operands = document.querySelector('#operands').value;
- if (desired.length === 0 || given.length === 0 || try.length === 0 || operands.length === 0) {
-  alert("Alle Felder müssen ausgefüllt sein!");
-  return;
- }
- operands = operands.split(';');
- for(let i=0; i < (int) try; i++) {
+function calculate(){
+  let desired = parseInt(document.querySelector('#desired').value);
+  let given = parseInt(document.querySelector('#given').value);
+  let trys = parseInt(document.querySelector('#try').value);
+  let operands = document.querySelector('#operands').value;
+  if (isNaN(desired) || isNaN(given) || isNaN(trys) || operands.length === 0) {
+    alert("Alle Felder müssen ausgefüllt sein!");
+    return;
+  }
+  operands = operands.split(';');
+  operations = [];
+  operants.forEach(element => {
+    if(element.length = 2){
+      operations.push({
+        operand: element[0],
+        value: element[1],
+      });
+    }
+    if(element.contains('=>')) {
+      split = element.split('=>');
+      operations.push({
+        operand: 'replace',
+        from: split[0],
+        to: split[1],
+      })
+    }
+  });
+  possible_ops = [];
+  for(let i=0; i < trys; i++) {
+    for(let l=0; l < operations; l++) {
+
+    }
+  }
+}
+function getAllOperations(start, others){
   
- }
 }
